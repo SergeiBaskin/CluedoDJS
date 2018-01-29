0cf42617c5262a14eff218061ab9e36750231787
dylan8 <dylan.kearns@ucdconnect.ie>
2018-01-29 16:59:23 +0000

    Update Board.java

--- Board.java
+++ Board.java
@@ -28,7 +28,7 @@ public class Board extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters
+        g.drawImage(image, 0, 0, null); // see javadoc for more info on the parameters
     }
 
 
